6f21793107f02acb7ed5aff007946f3373e1e72d
Rojin Saji <rojinsaji@Rojins-MacBook-Air.local>
2025-03-12 21:30:30 +0530

    comma issue near allowed host resolver

--- zenbackend/zenbackend/settings.py
+++ zenbackend/zenbackend/settings.py
@@ -31,8 +31,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ['localhost', '44.202.131.14']
-
+ALLOWED_HOSTS = ['localhost','127.0.0.1','44.202.131.14']
 
 # Application definition
 
