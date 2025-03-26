b52e6b37417b386c123322109401566f9f21bf47
Rojin Saji <rojinsaji@Rojins-MacBook-Air.local>
2025-03-26 21:01:36 +0530

    error fixed

--- zenbackend/accounts/tasks.py
+++ zenbackend/accounts/tasks.py
@@ -1,7 +1,7 @@
 # your_app_name/tasks.py
 import requests
 from celery import shared_task
-from accounts.utils import fetch_and_store_google_campaigns, scheduled_appointment_sync, fetch_campaigns_facebook, fetch_calls_for_last_days,appoinment_fetch_usage
+from accounts.utils import fetch_and_store_google_campaigns, fetch_campaigns_facebook, fetch_calls_for_last_days,appoinment_fetch_usage
 
 @shared_task
 def make_api_call():
